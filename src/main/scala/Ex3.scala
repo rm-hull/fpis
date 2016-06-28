6701ab1cc9bac852da2beb0d0083fa6d053004aa
Richard Hull <rm_hull@yahoo.co.uk>
2016-06-28 23:33:49 +0100

    Comment formatting

--- src/main/scala/Ex3.scala
+++ src/main/scala/Ex3.scala
@@ -20,42 +20,42 @@ object List {
     else Cons(as.head, apply(as.tail: _*))
     
    /**
-    * Exercise 3.1
-    * ============
-    * <see test>
-    */
+     * Exercise 3.1
+     * ============
+     * <see test>
+     **/
     
     
    /**
-    * Exercise 3.2
-    * ============
-    * Implement the function `tail` for removing the first element of a `List`.
-    * Note that the function takes constant time. What are the different choices
-    * you could make in your implementation if the `List` is `Nil`?
-    */
+     * Exercise 3.2
+     * ============
+     * Implement the function `tail` for removing the first element of a `List`.
+     * Note that the function takes constant time. What are the different choices
+     * you could make in your implementation if the `List` is `Nil`?
+     **/
    def tail[A](as: List[A]) = as match {
     case Nil => Nil
     case Cons(x, xs) => xs
   }
   
   /**
-   * Exercise 3.3
-   * ============
-   * Using the same idea, implement the function `setHead` for replacing the first
-   * element of a list with a different value.
-   */
+    * Exercise 3.3
+    * ============
+    * Using the same idea, implement the function `setHead` for replacing the first
+    * element of a list with a different value.
+    **/
   def setHead[A](newHead: A, as: List[A]) = as match {
     case Nil => List(newHead)
     case Cons(x, xs) => Cons(newHead, xs)
   }
   
   /**
-   * Exercise 3.4
-   * ============
-   * Generalize `tail` to the function `drop`, which removes the first `n` elements
-   * from a list. Note that this function takes time proportional only to the number
-   * of elements being dropped--we don't need to make a copy of the entire `List`.
-   */
+    * Exercise 3.4
+    * ============
+    * Generalize `tail` to the function `drop`, which removes the first `n` elements
+    * from a list. Note that this function takes time proportional only to the number
+    * of elements being dropped--we don't need to make a copy of the entire `List`.
+    **/
   def drop[A](l: List[A], n: Int): List[A] = { 
     require(n >= 0)
     n match {
@@ -70,11 +70,11 @@ object List {
   def tail_v2[A](as: List[A]) = drop(as, 1)
   
   /**
-   * Exercise 3.5
-   * ============
-   * Implement `dropWhile`, which removes elements from the `List` prefix as long
-   * as they match a predicate.
-   */
+    * Exercise 3.5
+    * ============
+    * Implement `dropWhile`, which removes elements from the `List` prefix as long
+    * as they match a predicate.
+    **/
   def dropWhile[A](l: List[A], f: A => Boolean): List[A] = l match {
     case Nil => Nil
     case Cons(x, xs) => if (f(x)) dropWhile(xs, f) else l
